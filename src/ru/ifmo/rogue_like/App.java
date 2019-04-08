91dd12dc6c746f7dcbc43ec4ac0a6c8296ec79f4
stan <sychevstanislav@gmail.com>
2019-04-08 16:41:12 +0300

    make frame in cammeraRender

--- src/ru/ifmo/rogue_like/App.java
+++ src/ru/ifmo/rogue_like/App.java
@@ -1,33 +1,50 @@
 package ru.ifmo.rogue_like;
 
+import ru.ifmo.rogue_like.map.RandomMap;
 import ru.ifmo.rogue_like.menu.Menu;
 import ru.ifmo.rogue_like.menu.MenuEntry;
+import ru.ifmo.rogue_like.player.Player;
+import ru.ifmo.rogue_like.player.PlayerListener;
 import ru.ifmo.rogue_like.rendering_system.CameraRenderer;
 import ru.ifmo.rogue_like.rendering_system.camera.Camera;
 import ru.ifmo.rogue_like.rendering_system.camera.ICamera;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public void newGame() {
-        ICamera camera = new Camera(40, 40, 0, 0);
+        ICamera camera = new Camera(41, 41, 0, 0);
+        camera.addRenderableObject(new RandomMap());
+        camera.addRenderableObject(Player.getInstanse());
+        PlayerListener listener = new PlayerListener(camera);
         CameraRenderer renderer = new CameraRenderer(camera);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
+            renderer.render();
             while (true) {
                 Thread.sleep(16);
-                camera.update(System.currentTimeMillis());
-                renderer.render();
+                String line = reader.readLine();
+                if (line != null) {
+                    for (char c : line.toCharArray()) {
+                        listener.keyPressed(c);
+                    }
+                    camera.update(System.currentTimeMillis());
+                    renderer.render();
+                }
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void start() throws IOException {
-        MenuEntry newGame = new MenuEntry("New Game", this::newGame);
-        Menu menu = new Menu(List.of(newGame));
-        menu.showMenu();
         newGame();
     }
 }
+
