f7a03857a1d82355a9f33b92d5227e83bdde5966
stan <sychevstanislav@gmail.com>
2019-04-10 17:02:00 +0300

    add frame

--- src/ru/ifmo/rogue_like/rendering_system/CameraRenderer.java
+++ src/ru/ifmo/rogue_like/rendering_system/CameraRenderer.java
@@ -2,31 +2,41 @@ package ru.ifmo.rogue_like.rendering_system;
 
 import ru.ifmo.rogue_like.rendering_system.camera.ICamera;
 
+import javax.swing.*;
 import java.io.IOException;
 
-public class CameraRenderer implements IRenderer {
+public class CameraRenderer extends JFrame implements IRenderer {
     ICamera camera;
 
+    private JLabel canvas;
+
     public CameraRenderer(ICamera camera) {
+        super("RougeLike");
+        canvas = new JLabel();
         this.camera = camera;
     }
 
     private void clear() throws IOException {
-        Runtime.getRuntime().exec("clear");
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+//        Runtime.getRuntime().exec("clear");
+//        System.out.print("\033[H\033[2J");
+//        System.out.flush();
+        canvas.setText("");
     }
 
 
     @Override
     public void render() throws IOException {
         clear();
+        StringBuilder text = new StringBuilder();
         char[][] view = camera.getView();
         for (char[] row : view) {
             for (char elem : row) {
-                System.out.print(elem);
+               // System.out.print(elem);
+                text.append(elem);
             }
-            System.out.println();
+            //System.out.println();
+            text.append("\n");
         }
+        canvas.setText(text.toString());
     }
 }
