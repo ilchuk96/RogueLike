0dcf318bbce729b5ff22b8010d3f5decf3488aeb
ValchukDmitry <dmitry.valchuk@7bits.it>
2019-06-18 17:36:54 +0300

    implement new commands execution system

---
+++ src/ru/ifmo/roguelike/CommandsExecutor.java
@@ -0,0 +1,59 @@
+package ru.ifmo.roguelike;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.ifmo.roguelike.commands.ICommand;
+import ru.ifmo.roguelike.commands.generators.ICommandGenerator;
+import ru.ifmo.roguelike.heroes.IHeroesService;
+import ru.ifmo.roguelike.heroes.mobs.IHero;
+import ru.ifmo.roguelike.render.CameraRenderer;
+import ru.ifmo.roguelike.render.camera.ICamera;
+
+public class CommandsExecutor implements Runnable {
+    private List<ICommandGenerator> commandsGenerators;
+    private CameraRenderer renderer;
+    private IHeroesService heroesService;
+    private ICamera camera;
+
+    public CommandsExecutor(List<ICommandGenerator> commandsGenerators,
+                            CameraRenderer renderer,
+                            IHeroesService heroesService,
+                            ICamera camera) {
+        this.commandsGenerators = commandsGenerators;
+        this.renderer = renderer;
+        this.heroesService = heroesService;
+        this.camera = camera;
+    }
+
+    public void update() {
+        List<ICommandGenerator> newCommandGenerators = new ArrayList<>();
+        for (ICommandGenerator commandGenerator : commandsGenerators) {
+            newCommandGenerators.addAll(applyCommand(commandGenerator));
+            this.renderer.render();
+        }
+        this.commandsGenerators.addAll(newCommandGenerators);
+        for (IHero h : heroesService.heroes()) {
+            camera.addRenderableObject(h);
+        }
+        this.renderer.render();
+        if (heroesService.getPlayer().isDead()) {
+            System.exit(0);
+        }
+    }
+
+    public List<ICommandGenerator> applyCommand(ICommandGenerator commandGenerator) {
+        while (!commandGenerator.isReady()) {
+        }
+        ICommand command = commandGenerator.getCommand();
+        List<ICommandGenerator> commandGenerators = command.apply();
+        return commandGenerators;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            update();
+        }
+    }
+}
